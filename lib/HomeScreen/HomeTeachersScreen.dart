162112f72264de0f7290bac3842f7f47330fd424
NOUR <noureldeenderna@gmail.com>
2023-04-08 02:10:02 +0200

    Delete Posts

--- lib/HomeScreen/HomeTeachersScreen.dart
+++ lib/HomeScreen/HomeTeachersScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:students_mobile/Profile/Profile.dart';
 import 'package:students_mobile/UI/Teachersbar.dart';
 import 'package:students_mobile/UI/minu.dart';
+import 'package:students_mobile/Utiils/User.dart';
 import 'package:students_mobile/Utiils/colors.dart';
 import 'package:students_mobile/Utiils/common_widgets.dart';
 
@@ -39,14 +40,21 @@ class _HomeTeachersScreenState extends State<HomeTeachersScreen> {
             ),
             Flexible(
               child: StreamBuilder(
-                stream: posts.snapshots(), //build connection
+                stream: FirebaseFirestore.instance
+                    .collection('posts')
+                    .where("teachers_name", isEqualTo:  Teachers.name)
+
+                    .snapshots(), //build connection
                 builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
                   if (streamSnapshot.hasData) {
                     return ListView.builder(
+
                       itemCount: streamSnapshot.data!.docs.length,  //number of rows
                       itemBuilder: (context, index) {
+
                         final DocumentSnapshot documentSnapshot =
                         streamSnapshot.data!.docs[index];
+                        final item =streamSnapshot.data!.docs[index];
                         return Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -62,70 +70,92 @@ class _HomeTeachersScreenState extends State<HomeTeachersScreen> {
                                           InkWell(
                                             child: Container(
                                               width: 380,
-                                              child: Card(
-                                                shape: const RoundedRectangleBorder(
-                                                    borderRadius: BorderRadius.all(
-                                                      Radius.circular(20.0),
-                                                    )),
-                                                color: Colors.grey[50],
-                                                child: Column(
-                                                  children: [
-                                                    SizedBox(
+                                              child: Dismissible(
 
-                                                      child: Column(
+                                                onDismissed: (_) {
+                                                  setState(() {
+                                                    final collection = FirebaseFirestore.instance.collection('posts').doc(documentSnapshot.id).delete().then((value) {
+                                                      ScaffoldMessenger.of(context)
+                                                          .showSnackBar(SnackBar(content: Text('تم حذف المنشور'),backgroundColor: Colors.red,));
+                                                    },);
 
-                                                        children: [
-                                                          Padding(
-                                                            padding: const EdgeInsets.all(8.0),
-                                                            child: Row(
-                                                              children:  [
-                                                                const CircleAvatar(
-                                                                  radius: 20, // Image radius
-                                                                  backgroundImage: AssetImage('assets/images/teacher.png'),
-                                                                ),
-                                                                const SizedBox(width: 10,),
-                                                                mediumText( documentSnapshot['teachers_name'].toString(), ColorResources.grey777, 16)
+                                                  });
+                                                },
+                                                key: UniqueKey(),
+                                                direction: DismissDirection.endToStart,
+                                                background: Container(
 
+                                                  color: Colors.red,
+                                                  margin:  EdgeInsets.symmetric(horizontal: 15),
+                                                  alignment: Alignment.centerLeft,
 
-                                                              ],
-                                                            ),
-                                                          ),
-                                                          Padding(
-                                                            padding: const EdgeInsets.all(8.0),
-                                                            child: Row(
-                                                              children:   [
-                                                                SizedBox(
-                                                                    width: 350,
-                                                                    child: Text(documentSnapshot['content'].toString(),style: TextStyle(color: defaultColor,fontSize: 15,),maxLines: 5)),
+                                                ),
+                                                child: Card(
+                                                  shape: const RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.all(
+                                                        Radius.circular(20.0),
+                                                      )),
+                                                  color: Colors.grey[50],
+                                                  child: Column(
+                                                    children: [
+                                                      SizedBox(
 
-                                                              ],
-                                                            ),
-                                                          ),
-                                                          Padding(
-                                                            padding: const EdgeInsets.all(8.0),
-                                                            child: Row(
-                                                              mainAxisAlignment: MainAxisAlignment.center,
-                                                              children:   [
-                                                                SizedBox(
-                                                                  width: 250,
-                                                                  height: 250,
-                                                                  child: FittedBox(
-                                                                    fit: BoxFit.fill,
-                                                                    child: Image.network(documentSnapshot['image']),
+                                                        child: Column(
+
+                                                          children: [
+                                                            Padding(
+                                                              padding: const EdgeInsets.all(8.0),
+                                                              child: Row(
+                                                                children:  [
+                                                                  const CircleAvatar(
+                                                                    radius: 20, // Image radius
+                                                                    backgroundImage: AssetImage('assets/images/teacher.png'),
                                                                   ),
-                                                                ),
+                                                                  const SizedBox(width: 10,),
+                                                                  mediumText( documentSnapshot['teachers_name'].toString(), ColorResources.grey777, 16)
 
 
-                                                              ],
+                                                                ],
+                                                              ),
                                                             ),
-                                                          ),
+                                                            Padding(
+                                                              padding: const EdgeInsets.all(8.0),
+                                                              child: Row(
+                                                                children:   [
+                                                                  SizedBox(
+                                                                      width: 350,
+                                                                      child: Text(documentSnapshot['content'].toString(),style: TextStyle(color: defaultColor,fontSize: 15,),maxLines: 5)),
 
-                                                        ],
+                                                                ],
+                                                              ),
+                                                            ),
+                                                            Padding(
+                                                              padding: const EdgeInsets.all(8.0),
+                                                              child: Row(
+                                                                mainAxisAlignment: MainAxisAlignment.center,
+                                                                children:   [
+                                                                  SizedBox(
+                                                                    width: 250,
+                                                                    height: 250,
+                                                                    child: FittedBox(
+                                                                      fit: BoxFit.fill,
+                                                                      child: Image.network(documentSnapshot['image']),
+                                                                    ),
+                                                                  ),
+
+
+                                                                ],
+                                                              ),
+                                                            ),
+
+                                                          ],
+                                                        ),
+
                                                       ),
 
-                                                    ),
+                                                    ],
+                                                  ),
 
-                                                  ],
                                                 ),
 
                                               ),
